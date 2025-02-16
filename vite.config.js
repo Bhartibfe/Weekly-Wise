b2252a58c81eeaf681c97ece9af847769d5f644e
Bhartibfe <140871863+Bhartibfe@users.noreply.github.com>
2025-02-16 13:29:52 +0530

    viteconfig change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'react-router-dom',
+      'lucide-react',
+      'prop-types',
+      '@hello-pangea/dnd'
+    ]
+  },
+  build: {
+    commonjsOptions: {
+      include: [
+        /node_modules/,
+        /hello-pangea/
+      ]
+    }
+  }
 })
